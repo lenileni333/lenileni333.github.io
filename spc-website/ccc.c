e3ee36c94f3263dab1987f6ab8ddd34dd83011c3
lenileni333 <danielbarkho06@gmail.com>
2026-09-16 13:24:25 +1200

    Add files via upload

---
+++ spc-website/ccc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i <= 1000; i++) {
+        printf("FISH!FISH!FISH!FISH!FISH!FISH!FISH!FISH!FISH!FISH!FISH!FISH!FISH!FISH!\n");
+    }
+    return 0;
+}
